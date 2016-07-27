93a6137fd74c4af427d74296bdcb592686feb2a0
dehbod <dehbod@gmail.com>
2016-07-27 18:55:38 +0430

    R code

---
+++ R/KmlProcess.R
@@ -0,0 +1,75 @@
+rm(list = ls())
+library(XML);
+library(geosphere);
+library(RODBC);
+
+
+files = dir(pattern = '[^\\.R]$');
+
+for(ii in 1:length(files)) {
+
+
+xmlFile <- xmlParse(file = files[ii]);
+
+tracks <- xpathSApply(
+  xmlFile,
+  '/x:kml/x:Document/x:Folder[@id = "Tracks"]/x:Placemark/x:MultiGeometry/x:LineString/x:coordinates/text()',
+  xmlValue,
+  namespaces = 'x'
+);
+
+tracks_name <-
+  xpathSApply(
+    xmlFile,
+    '/x:kml/x:Document/x:Folder[@id = "Tracks"]/x:Placemark/x:name/text()',
+    xmlValue,
+    namespaces = 'x'
+  );
+
+coordinates = strsplit(tracks[1], ' ')[[1]]
+
+coordinates = strsplit(coordinates, ',')
+
+
+trackData = data.frame(
+  longitude = double(length(coordinates)),
+  latitude = double(length(coordinates)),
+  elevation = double(length(coordinates))
+)
+
+
+for (i in 1:length(coordinates)) {
+  trackData$longitude[i] = as.double(coordinates[[i]][1])
+  
+  trackData$latitude[i] = as.double(coordinates[[i]][2])
+  
+  trackData$elevation[i] = as.double(coordinates[[i]][3])
+  
+}
+
+
+A <- as.matrix(trackData[1:(nrow(trackData)-1), 1:2]);
+B <- as.matrix(trackData[2:nrow(trackData), 1:2]);
+
+C = distVincentyEllipsoid(
+  p1 = A,
+  p2 = B,
+  a = 6378137,
+  b = 6356752.3142,
+  f = 1 / 298.257223563
+);
+
+trackData$distance = c(0,C);
+trackData$totalDistance = cumsum(trackData$distance);
+
+#ellipsoid             a              b                f
+#WGS84                 6378137        6356752.3142     1/298.257223563
+
+# plot(trackData$totalDistance, trackData$elevation, type = 'l')
+
+str <- paste(as.character(trackData$totalDistance), as.character(trackData$elevation), sep = ',');
+str <- paste(str, collapse = ' ');
+
+
+
+}
