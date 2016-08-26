bc980e08736b551fcb2008b7991ed3c3f045e9e5
farshad <farsh_ad@outlook.com>
2016-08-26 15:26:09 +0430

    Make responsive

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,5 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var paths = {
+    less: '../src/less/*.less'
+};
+
+gulp.task('default', ['watch', 'less']);
+
+gulp.task('less', function() {
+    gulp
+        .src(paths.less)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('lazzor.css', { newLine: '\r\n' }))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('../src/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.less, ['less']);
+}).on('stop', function (e) {
+    console.log('watching...');
 });
